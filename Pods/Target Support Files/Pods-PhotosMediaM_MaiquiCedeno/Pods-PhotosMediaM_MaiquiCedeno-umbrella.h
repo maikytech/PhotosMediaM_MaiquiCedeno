050a3458c49eeef5aeda2b4583953ce404fd306b
maikytech <maikytech1307@gmail.com>
2021-05-08 17:51:54 -0500

    Add cocoapods

---
+++ Pods/Target Support Files/Pods-PhotosMediaM_MaiquiCedeno/Pods-PhotosMediaM_MaiquiCedeno-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_PhotosMediaM_MaiquiCedenoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PhotosMediaM_MaiquiCedenoVersionString[];
+
