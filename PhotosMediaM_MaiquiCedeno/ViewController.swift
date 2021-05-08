a8c14e4c7b755a0674a8c538d23dc02c4c4339df
maikytech <maikytech1307@gmail.com>
2021-05-08 17:26:12 -0500

    Start Project

---
+++ PhotosMediaM_MaiquiCedeno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosMediaM_MaiquiCedeno
+//
+//  Created by Maiqui Cedeño on 8/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
